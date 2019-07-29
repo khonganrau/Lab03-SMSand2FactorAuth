ec4d5ab864af91702663fc117afd9beb9dc6b901
duongmanhquynh <junie.fancy@gmail.com>
2019-07-29 15:47:21 +0700

    commit

---
+++ MvcPWxTest/MvcPWxTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcPWxTest.MvcApplication" Language="C#" %>
---
+++ MvcPWxTest/MvcPWxTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcPWxTest.Startup))]
+namespace MvcPWxTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
